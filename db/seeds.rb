d98c1ccf8603448b09a1b691f7b6425ce94ef49f
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2017-03-22 14:42:38 +0100

    Added basic functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "2123435435",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number: "45235234554",
+    category:     "chinese"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "3453434",
+    category:     "french"
+  },
+  {
+    name:         "The Butcher",
+    address:      "Amsterdam North",
+    phone_number: "4355534",
+    category:     "belgian"
+  },
+  {
+    name:         "Pizzeria",
+    address:      "Amsterdam Centraal",
+    phone_number: "348510495713895",
+    category:     "italian"
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
