90ffbe8c63214cc967f21726504742e1e7277bd3
Anu Maria Kokkarinen <anu-maria@hotmail.com>
2017-03-22 11:45:18 +0100

    Change phone_number format from integer to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170322103554) do
+ActiveRecord::Schema.define(version: 20170322104253) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "address"
-    t.integer  "phone_number"
+    t.string   "phone_number"
     t.string   "category"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
